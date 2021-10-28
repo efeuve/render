e07d2b1abf53e118e917e51fe69b497ae3faad52
Felipe <fillipvt@gmail.com>
2021-10-28 08:57:24 -0500

    add link and hover on button

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ const component = html`
       </div>
       <div class="go-header"><span>Get our favorite casino bonus</span></div>
       <div class="go-card">
-        <div class="logo-bg">
-          <img class="us-flag" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1634930030/US_flag_pwa46n.png" alt="" />
-          <img class="logo" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1635290787/las_atlantis_bg_dark_ms3bdr.png" alt="" />
-        </div>
+        <a class="logo-bg" data-action="Bonus Page Top CTA Logo" href="https://www.onlineunitedstatescasinos.com/go/lasatlantis-bonuses/">
+          <img class="us-flag" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1634930030/US_flag_pwa46n.png" alt="US Flag" />
+          <img class="logo" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1635290787/las_atlantis_bg_dark_ms3bdr.png" alt="Las Atlantis Casino" />
+        </a>
         <div class="cta">
           <div class="bonus">Exclusive $12,500</div>
           <div class="sub-heading">Deposit Bonus</div>
-          <a class="button">Claim your bonus now</a>
+          <a data-action="Bonus Page Top CTA Button" href="https://www.onlineunitedstatescasinos.com/go/lasatlantis-bonuses/" class="button">Claim your bonus now</a>
         </div>
       </div>
-      <a class="go-sublink">View All Of Our Favorite Bonuses</div>
+      <a class="go-sublink" href="/bonuses/#best-bonus-table">View All Of Our Favorite Bonuses</div>
   </div>
 `;
 const elem = document.getElementById("preact-container");
