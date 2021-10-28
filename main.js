ad01fede129c83883d9452bb21b2b5b0a0b8cdfa
Felipe <fillipvt@gmail.com>
2021-10-28 08:39:27 -0500

    add responsiveness

--- main.js
+++ main.js
@@ -5,26 +5,22 @@ const html = htm.bind(vhtml);
 
 const component = html`
   <div class="go-container">
-    <div class="go-col">
-      <div class="go-left">
+      <div class="go-img">
         <img src="https://res.cloudinary.com/dhxw87xee/image/upload/v1634930000/bonus_image_uovs66.png" />
       </div>
-      <div class="go-right">
-        <div class="heading">Hello World!!!</div>
-        <div class="card">
-          <div class="logo-bg">
-            <img class="us-flag" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1634930030/US_flag_pwa46n.png" alt="" />
-            <img class="logo" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1634930014/las_atlantis_bg_dark_g05pkq.png" alt="" />
-            <a class="secondary">View All Of Our Favorite Bonuses</div>
-          </div>
-          <div class="cta">
-            <div class="bonus">Exclusive $12,500</div>
-            <div class="sub-heading">Deposit Bonus</div>
-            <a class="button">Claim your bonus now</a>
-          </div>
+      <div class="go-header"><span>Get our favorite casino bonus</span></div>
+      <div class="go-card">
+        <div class="logo-bg">
+          <img class="us-flag" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1634930030/US_flag_pwa46n.png" alt="" />
+          <img class="logo" src="https://res.cloudinary.com/dhxw87xee/image/upload/v1635290787/las_atlantis_bg_dark_ms3bdr.png" alt="" />
+        </div>
+        <div class="cta">
+          <div class="bonus">Exclusive $12,500</div>
+          <div class="sub-heading">Deposit Bonus</div>
+          <a class="button">Claim your bonus now</a>
         </div>
       </div>
-    </div>
+      <a class="go-sublink">View All Of Our Favorite Bonuses</div>
   </div>
 `;
 const elem = document.getElementById("preact-container");
